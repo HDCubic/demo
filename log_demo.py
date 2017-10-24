66de4697f91aeb4875386e64ec2973d90f90f522
HDCubic <2641292203@qq.com>
2017-10-25 01:13:35 +0800

    增加带线程的日志控制

--- log_demo.py
+++ log_demo.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import time
 import signal
 import logging
+import threading
 
 # 指定打印日志的级别
 logging.basicConfig(
@@ -31,8 +33,33 @@ def set_log_debug(a, b):
     print a, b, 'set debug'
     LOG.setLevel(logging.DEBUG)
 
-signal.signal(signal.SIGUSR1, set_log_error)
-signal.signal(signal.SIGUSR2, set_log_debug)
+signal.signal(signal.SIGUSR1, set_log_debug)
+signal.signal(signal.SIGUSR2, set_log_error)
+
+class myThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            LOG.error('11111111111')
+            LOG.warn('22222222222')
+            LOG.info('33333333333')
+            LOG.debug('44444444444')
+            time.sleep(1)
+
+def handler(signum, frame):
+    print("exit")
+    sys.exit()
+
+threads = []
+for _ in range(0,2):
+    t = myThread()
+    t.setDaemon(True)
+    threads.append(t)
+    t.start()
+#for _ in range(0,2):
+#    t.join()
 
 while True:
     LOG.error('11111111111')
